f72758c420718ff3c2d9a28d195b4d481df4d167
Yang jun mo <abc22842199@gmail.com>
2026-05-18 17:16:08 +0900

    Initial commit

---
+++ src/main/java/hanium/damso/DamsoApplication.java
@@ -0,0 +1,13 @@
+package hanium.damso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DamsoApplication.class, args);
+    }
+
+}
